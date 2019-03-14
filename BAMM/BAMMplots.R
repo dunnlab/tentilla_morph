f096b0d90639f3d2be3c8e506bb5a79a3033be8e
antropoteuthis <alejandro_damianserrano@brown.edu>
2019-03-14 15:26:53 -0400

    added intro anatomy figure

--- BAMM/BAMMplots.R
+++ BAMM/BAMMplots.R
@@ -4,7 +4,7 @@ library(geiger)
 library(coda)
 setwd("~/tentilla_morph/BAMM")
 # load tree
-tree <- read.tree("heteroneme_elongation_tree.tre")
+tree <- read.tree("desmoneme_elongation_tree.tre")
 
 # ESS after 20% burnin
 mcmcout <- read.csv("mcmc_out.txt")
@@ -19,18 +19,18 @@ summary(edata)
 
 # the 6 most likely scenario's within the 95% credible scenarios
 # F value is the relative probability, F values from all scenarios add up to F=1
-pdf("BAMM_heteroneme_elongation_css.pdf", width=7, height=10)
+pdf("BAMM_desmoneme_elongation_css.pdf", width=7, height=10)
 css <- credibleShiftSet(edata, expectedNumberOfShifts = 1, threshold=5, set.limit=0.95)
 BAMMtools:::plot.credibleshiftset(css, plotmax = 6, pal = "temperature", legend=T, use.plot.bammdata = T)
 dev.off()
 
 # net diversification graph
-pdf("BAMM_heteroneme_elongation_netdiv.pdf", width=7, height=5)
+pdf("BAMM_desmoneme_elongation_netdiv.pdf", width=7, height=5)
 starttime <- max(branching.times(tree))
 plotRateThroughTime(edata, intervalCol="darkgreen", avgCol="darkgreen", start.time=starttime, ylim=c(0,25))
 dev.off()
 
-pdf("heteroneme_elongation_ratesplot.pdf",width=16,height=16)
+pdf("desmoneme_elongation_ratesplot.pdf",width=16,height=16)
 plot.bammdata(edata,lwd=2,legend=T,labels=T,cex=0.9,logcolor=T,breaksmethod="jenks",pal=c("#0D0887", "#CC4678", "#F0F921"))
 dev.off()
 
--- BAMM/BAMMsetpriors.R
+++ BAMM/BAMMsetpriors.R
@@ -2,14 +2,14 @@ library(BAMMtools)
 library(stringr)
 library(readr)
 setwd("~/tentilla_morph/BAMM")
-tree <- read.tree("heteroneme_elongation_tree.tre")
-traits <- read.csv("heteroneme_elongation.txt", sep="\t", header = F)
+tree <- read.tree("desmoneme_elongation_tree.tre")
+traits <- read.csv("desmoneme_elongation.txt", sep="\t", header = F)
 rownames(traits) = traits[,1]
 traits <- traits[tree$tip.label,]
-write_tsv(traits, "heteroneme_elongation.txt", col_names = FALSE)
+write_tsv(traits, "desmoneme_elongation.txt", col_names = FALSE)
 is.ultrametric(tree) # check for ultrametric
 is.binary.tree(tree) # check if there are no polytomies
 min(tree$edge.length) # check all branch lengths are > 0
-setBAMMpriors(phy = tree, traits = "heteroneme_elongation.txt")
+setBAMMpriors(phy = tree, traits = "desmoneme_elongation.txt")
 setwd("~/tentilla_morph/")
 #q()
